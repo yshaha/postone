89815c303cb01ae9a2b28ffebfc4b40e300cf49c
yshaha <yshaha79@gmail.com>
2026-06-03 11:56:17 +0900

    login url 수정

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 AUTHENTICATION_BACKENDS = [
